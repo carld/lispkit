f7134dab161c045fc9d636e4014efd8e59826a0e
Carl Douglas <carl.douglas@gmail.com>
2011-10-15 00:49:08 +1100

    fix ackerman function

--- examples/ackermann.lisp
+++ examples/ackermann.lisp
@@ -1,9 +1,7 @@
 ( LETREC ACKERMANN
-    ( ACKERMANN LAMBDA ( X Y )
-     ( IF ( EQ X ( QUOTE 0 ) )
-     ( ADD Y ( QUOTE 1 ) )
-     ( IF ( EQ Y ( QUOTE 0 ) )
-     ( ACKERMANN ( SUB X ( QUOTE 1 ) ) ( QUOTE 1 ) )
-     ( ACKERMANN ( SUB X ( QUOTE 1 ) ) ( ACKERMANN X ( SUB Y (
-       QUOTE 1 ) ) ) ) ) ) ) )                                                                                                                               QUOTE 1 ) ) ) ) ) ) ) )
-
+ ( ACKERMANN LAMBDA ( X Y )
+  ( IF ( EQ X ( QUOTE 0 ) )
+    ( ADD Y ( QUOTE 1 ) )
+    ( IF ( EQ Y ( QUOTE 0 ) )
+      ( ACKERMANN ( SUB X ( QUOTE 1 ) ) ( QUOTE 1 ) )
+      ( ACKERMANN ( SUB X ( QUOTE 1 ) ) ( ACKERMANN X ( SUB Y ( QUOTE 1 ) ) ) ) ) ) ) ) 
