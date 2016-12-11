c393c69a0e128bf4c08f71bed6e4a8e611518a86
Carl <carl.douglas@gmail.com>
2016-12-11 21:03:22 +1300

    Use variables to point to source and secd machine Lispkit compilers.

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 CMD=./lispkit
+COMPILER_SRC=./compiler.lisp
+COMPILER_OBJ=./compiler.secd
 
 for f in `ls tests/execute-??`
 do
@@ -12,11 +14,11 @@ done
 for f in `ls tests/compile-??`
 do
   echo "Running compiler test $f ..."
-  if [ ! -f compiler.ascii ]; then echo "no compiler" && exit -1; fi;
-  $CMD compiler.ascii $f
+  if [ ! -f $COMPILER_OBJ ]; then echo "no compiler" && exit -1; fi;
+  $CMD $COMPILER_OBJ $f
   if [ $? -ne 0 ]; then exit -1; fi;
 done
 
 echo "Bootstrapping ..."
-$CMD compiler.ascii compiler.txt.ascii
+$CMD $COMPILER_OBJ $COMPILER_SRC
 
